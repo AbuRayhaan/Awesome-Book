244f43f6c8e4e4f440ada6f4ef676f26d8286b53
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-04-21 09:09:13 +0100

    Modified the scriptClass.js file

--- scriptClass.js
+++ scriptClass.js
@@ -107,3 +107,39 @@ if (books !== null) {
     displayBooks(book.id, book.title, book.author);
   });
 }
+
+/*  NAVIGATION-BAR  */
+const date = document.getElementById('date');
+const dateNow = new Date();
+date.innerText = dateNow;
+
+const listBooks = document.getElementById('listBooks');
+const addBook = document.getElementById('addBook');
+const contactUs = document.getElementById('contactInfo');
+
+listBooks.addEventListener('click', () => {
+  listBooks.classList.add('active');
+  addBook.classList.remove('active');
+  contactUs.classList.remove('active');
+  document.getElementById('bookList-container').classList.remove('hide');
+  document.getElementById('addBook-container').classList.add('hide');
+  document.getElementById('contactUs-container').classList.add('hide');
+});
+
+addBook.addEventListener('click', () => {
+  listBooks.classList.remove('active');
+  addBook.classList.add('active');
+  contactUs.classList.remove('active');
+  document.getElementById('addBook-container').classList.remove('hide');
+  document.getElementById('bookList-container').classList.add('hide');
+  document.getElementById('contactUs-container').classList.add('hide');
+});
+
+contactUs.addEventListener('click', () => {
+  listBooks.classList.remove('active');
+  addBook.classList.remove('active');
+  contactUs.classList.add('active');
+  document.getElementById('contactUs-container').classList.remove('hide');
+  document.getElementById('bookList-container').classList.add('hide');
+  document.getElementById('addBook-container').classList.add('hide');
+});
